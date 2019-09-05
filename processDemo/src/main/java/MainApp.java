cd08ad102e72bbc9225147e91bb0ec7f74bb89d3
fhochleitner <felix.hochleitner@gepardec.com>
2019-09-05 11:26:37 +0200

    ProcessDemo ExitCode & alle Prozesse ausgeben

--- processDemo/src/main/java/MainApp.java
+++ processDemo/src/main/java/MainApp.java
@@ -1,12 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+
 public class MainApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         ProcessHandle.allProcesses().map(p -> p.info().command()).filter(command -> command.isPresent())
                 .forEach(command -> System.out.println(command));
 
         System.out.println("\n-------------------------------------\n");
-        ProcessBuilder.Redirect
+        ProcessBuilder pb = new ProcessBuilder("java", "-version");
+        pb.redirectErrorStream(true);
+        var process = pb.start();
+        process.getInputStream().transferTo(System.out);
+        process.waitFor();
+        process.onExit().thenAccept(p -> System.out.println("Prozess mit Wert : " + p.exitValue() + " beendet"));
 
     }
 }
