371de8ef0fa52ddf8c2deccc7e5d186e22d49dbb
fhochleitner <felix.hochleitner@gepardec.com>
2019-09-05 15:09:19 +0200

    StreamDemo

--- StreamDemo/src/main/java/com/gepardec/stream/Person.java
+++ StreamDemo/src/main/java/com/gepardec/stream/Person.java
@@ -3,6 +3,7 @@ package com.gepardec.stream;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.builder.ToStringBuilder;
 
 @Data
 @NoArgsConstructor
@@ -15,7 +16,7 @@ public class Person {
 
     @Override public String toString() {
 
-        return new org.apache.commons.lang3.builder.ToStringBuilder(this)
+        return new ToStringBuilder(this)
                 .append("id", id)
                 .append("firstName", firstName)
                 .append("lastName", lastName)
